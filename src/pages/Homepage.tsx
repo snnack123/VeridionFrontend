e2d656aae3192c613d8838d8f7060742e316d8bd
Lungu Mihai Adrian <lungu.snk199@gmail.com>
2023-11-19 21:54:22 +0200

    added env example

--- src/pages/Homepage.tsx
+++ src/pages/Homepage.tsx
@@ -5,7 +5,7 @@ import SearchSection from "../components/Company/Sections/SearchSection";
 const Homepage = () => {
   const token = useSelector((state: RootState) => state.user.token);
 
-  return <>{token ? <SearchSection /> : <div>Homepage</div>}</>;
+  return <>{token ? <SearchSection /> : <div></div>}</>;
 };
 
 export default Homepage;
